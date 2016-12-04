5e23bf8a1f279bcf9a0c94c6e388c546b564c397
Nicklas Winger <winger.nicklas@gmail.com>
2016-12-04 17:03:48 +0100

    Render videolist with data from youtube api

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/searchbar';
+import VideoList from './components/videolist';
 
 const API_KEY = 'AIzaSyBK9PojZ8JcgWgUl1GB8yHwwBikYLfGMvo';
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: []
+        };
 
-// Take components generated HTML and put it on the page (DOM)
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container'));
